165b0ba64e97e7fd4838c4f2dc4f96153bb3e515
thereview <thereview.vn@gmail.com>
2024-08-25 18:21:41 +0700

    push source

---
+++ scripts/proxy/utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x34fbAdf1ff63AfA5E57D2741CfB58d4E26973ef7'
+export const ProxyAdmin = '0x624ce14c6579D108fF3e00Eb6d02d350aa7FA4be'
+export const SWTRImplementation = '0x07C0AEc2266fABEf23D1743C6b8a6e6DD9B527b7'
+    
